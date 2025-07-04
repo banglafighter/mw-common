ccecfdcb1abc587985441e2f7636bc3640591046
H.M.Touhid Mia (HMTMCSE) <hmtm.cse@gmail.com>
2025-07-04 21:15:45 +0600

    Added Common Constant

---
+++ mw_common/__init__.py
@@ -0,0 +1,2 @@
+from mw_common.mw_const import HTTPContentType
+from mw_common.mw_const import HTTPMethod
---
+++ mw_common/mw_const.py
@@ -0,0 +1,12 @@
+class HTTPContentType:
+    APPLICATION_JSON = "application/json"
+    MULTIPART_FORM_DATA = "multipart/form-data"
+    FORM_DATA = "application/x-www-form-urlencoded"
+
+
+class HTTPMethod:
+    GET = "GET"
+    POST = "POST"
+    PUT = "PUT"
+    DELETE = "DELETE"
+    PATCH = "PATCH"
