fd888eae9e1195d626b8571c69ccac97bbb1f85a
H.M.Touhid Mia (HMTMCSE) <hmtm.cse@gmail.com>
2025-07-10 16:07:39 +0600

    Added Serializer and Deserializer and constant

--- mw_common/__init__.py
+++ mw_common/__init__.py
@@ -1,3 +1,5 @@
 from mw_common.mw_const import HTTPContentType
 from mw_common.mw_const import HTTPMethod
 from mw_common.pw_util import MwUtil
+from mw_common.object.sdlize import SDLize
+from mw_common.mw_const import HTTPStatusCode
--- mw_common/mw_const.py
+++ mw_common/mw_const.py
@@ -2,6 +2,7 @@ class HTTPContentType:
     APPLICATION_JSON = "application/json"
     MULTIPART_FORM_DATA = "multipart/form-data"
     FORM_DATA = "application/x-www-form-urlencoded"
+    APPLICATION_UNICODE_JSON = "application/json; charset=utf-8"
 
 
 class HTTPMethod:
@@ -10,3 +11,35 @@ class HTTPMethod:
     PUT = "PUT"
     DELETE = "DELETE"
     PATCH = "PATCH"
+
+
+class HTTPStatusCode:
+    # Informational 1xx
+    CONTINUE = 100
+    SWITCHING_PROTOCOLS = 101
+
+    # Success 2xx
+    OK = 200
+    CREATED = 201
+    ACCEPTED = 202
+    NO_CONTENT = 204
+
+    # Redirection 3xx
+    MOVED_PERMANENTLY = 301
+    FOUND = 302
+    NOT_MODIFIED = 304
+
+    # Client Error 4xx
+    BAD_REQUEST = 400
+    UNAUTHORIZED = 401
+    FORBIDDEN = 403
+    NOT_FOUND = 404
+    METHOD_NOT_ALLOWED = 405
+    CONFLICT = 409
+    UNPROCESSABLE_ENTITY = 422
+
+    # Server Error 5xx
+    INTERNAL_SERVER_ERROR = 500
+    NOT_IMPLEMENTED = 501
+    BAD_GATEWAY = 502
+    SERVICE_UNAVAILABLE = 503
